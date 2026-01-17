f6a9c81c2c2379ddf7d3b80a137a17c1f7cf4254
Othavio Nunes <ornsantos@sga.pucminas.com.br>
2026-01-17 07:24:07 -0300

    Etapa2-ScreenMatch

--- src/main/java/br/com/othavio/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/othavio/screenmatch/ScreenmatchApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.othavio.screenmatch.model.DadosSerie;
+import br.com.othavio.screenmatch.service.ConsumoApi;
+import br.com.othavio.screenmatch.service.ConverteDados;
+
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner{
 
@@ -13,6 +17,12 @@ public class ScreenmatchApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception{
-		System.out.println("Primeiro projeto Spring sem Web");
-	}
+		var ConsumoApi = new ConsumoApi();
+		var json = ConsumoApi.obterDados("https://www.omdbapi.com/?i=tt3896198&apikey=de092c47");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+}
+
 }
